a2ba7f31b7c7331bcddbe7842aa971c4f6f3ff29
Barathrajan <barathrajan.b@fcsonline.co.in>
2024-06-12 15:06:21 +0530

    initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container>
+    <v-flex>
+      <v-row>
+      <v-col cols="12" lg="4" md="6" sm="12">
+        <v-img class="img" src="../assets/car1.jpg"></v-img>
+      </v-col>
+      <v-col cols="12" lg="4" md="6" sm="12">
+        <v-img class="img" src="../assets/car2.jpg"></v-img>
+      </v-col>
+      <v-col cols="12" lg="4" md="6" sm="12"> 
+        <v-img class="img" src="../assets/car3.jpg"></v-img>
+      </v-col>
+    <!-- </v-row>
+    <v-row> -->
+      <v-col cols="12" lg="4" md="6" sm="12">
+        <v-img class="img" src="../assets/car7.jpg"></v-img>
+      </v-col>
+      <v-col cols="12" lg="4" md="6" sm="12">
+        <v-img class="img" src="../assets/car8.jpg"></v-img>
+      </v-col>
+      <v-col cols="12" lg="4" md="6" sm="12"> 
+        <v-img class="img" src="../assets/car9.jpg"></v-img>
+      </v-col>
+    <!-- </v-row>
+    <v-row> -->
+      <v-col cols="12" lg="4" md="6" sm="12">
+        <v-img class="img" src="../assets/car4.jpg"></v-img>
+      </v-col>
+      <v-col cols="12" lg="4" md="6" sm="12">
+        <v-img class="img" src="../assets/car5.jpg"></v-img>
+      </v-col>
+      <v-col cols="12" lg="4" md="6" sm="12"> 
+        <v-img class="img" src="../assets/car6.jpg"></v-img>
+      </v-col>
+    </v-row>
+    </v-flex>
+  </v-container>
+</template> 
+
+<style>
+.img{
+  height: 300px;
+  width: 800px;
+}
+</style>
+
+<script>
+export default {
+  name: "HelloWorld",
+  
+};
+</script>
